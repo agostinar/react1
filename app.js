c3ed01bc4ce5efe08b504e3429ac6e7c0c474296
agostinar <132631770+agostinar@users.noreply.github.com>
2023-08-26 12:09:46 -0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import NavBar from './components/NavBar';
+import ItemListContainer from './components/ItemListContainer';
+
+const App = () => {
+  return (
+    <div>
+      <NavBar />
+      <ItemListContainer greeting="¡Bienvenidos a ElectroStore!" />
+    </div>
+  );
+};
+
+export default App;
